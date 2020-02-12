3e00f84fc198142983435cb0f38c6dfa2fe30758
Marco Bacelo <marcobacelo90@gmail.com>
2020-02-11 22:45:29 -0300

    Update GraphQL Type

--- app/GraphQL/Types/TransactionType.php
+++ app/GraphQL/Types/TransactionType.php
@@ -1,30 +1,29 @@
 <?php
-
 namespace App\GraphQL\Types;
 
-use App\transactions;
+use App\Transaction;
 use GraphQL\Type\Definition\Type;
 use Rebing\GraphQL\Support\Type as GraphQLType;
 
-class TransactionType extends GraphQLType
+class WineType extends GraphQLType
 {
     protected $attributes = [
-        'name' => 'balance',
-        'description' => 'Lorem Ypsum',
-        'model' => transactions::class
+        'name' => 'Transaction',
+        'description' => 'Details about a bank account',
+        'model' => Transaction::class
     ];
 
-    public function fields()
+    public function fields(): array
     {
         return [
             'id' => [
                 'type' => Type::nonNull(Type::int()),
-                'description' => 'The id of a user'
+                'description' => 'Id of the balance',
             ],
-            'balance' => [
+            'name' => [
                 'type' => Type::nonNull(Type::int()),
-                'description' => 'The balance of a user'
-            ],
+                'description' => 'The balance of the bank account',
+            ]
         ];
     }
 }
