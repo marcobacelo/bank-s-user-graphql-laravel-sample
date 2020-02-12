adf0b69062d937de4d9b73b3a6ad1ef3ccf7e6c1
Marco Bacelo <marcobacelo90@gmail.com>
2020-02-11 21:32:20 -0300

    Add to script a command for publish the GraphQL

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker exec -it api-graphql-app composer install
+docker exec -it api-graphql-app php artisan vendor:publish --provider="Rebing\GraphQL\GraphQLServiceProvider"
 docker exec -it api-graphql-app php artisan migrate
 docker exec -it api-graphql-app php artisan db:seed
